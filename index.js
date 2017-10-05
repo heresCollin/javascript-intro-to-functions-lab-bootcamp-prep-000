402ccc7f01b6b5d97b3b8483a97dc90a1312e5ba
Collin Bradley <herescollin@gmail.com>
2017-10-05 21:22:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } elseif (string === string.toUpperCase()) {
+    return 'YES INDEED!'
+  } else {
+    return "I can't hear you!"
+  }
+}
